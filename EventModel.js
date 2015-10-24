f3ae4eb43046b8d2ee532815afc09a2b9f0f7f06
Cory Cook <clovecooks@gmail.com>
2015-10-24 13:45:13 -0700

    Added delegation

--- EventModel.js
+++ EventModel.js
@@ -1,87 +1,94 @@
-(function(global) {
-    "use strict";
+"use strict";
 
-    function EventModel(model) {
-        this.targets = [];
-        this.model = {};
-        this.pushmodel(model);
-    }
-    (function() {
-        function createEvent(type) {
-            try {
-                return new Event(type);
-            } catch(e) {
-                if (typeof document.createEvent !== "undefined") {
-                    var event = document.createEvent("Event");
-                    event.initEvent(type, true, true);
-                    return event;
-                }
-            }
-            return { type: type };
-        }
-        function wrapper(obj, handler, event) {
-            if (event == null)
-                event = { };
-            event.model = this;
-            handler.call(obj, event);
-        }
-        this.enumerate = function(target, callback) {
-            for (var _property in this.model) {
-                if (_property in target) {
-                    callback(target[_property], this.model[_property]);
-                }
-            }
-        };
-        this.pushmodel = function(model) {
-            for (var _property in model) {
-                if (!(_property in this.model))
-                    this.model[_property] = {};
-                for (var _handler in model[_property]) {
-                    this.model[_property][_handler] = model[_property][_handler];
-                    if (!(_handler in this))
-                        this[_handler] = this.trigger.bind(this, _handler);
-                }
-            }
-        };
-        this.attach = function(target) {
-            var self = this;
-            this.enumerate(target, function(obj, property) {
-                for (var _handler in property) {
-                    var fn = wrapper.bind(self, obj, property[_handler]);
-                    obj.addEventListener(_handler, fn);
-                }
-            });
-            this.targets.push(target);
-        };
-        this.trigger = function(type, data) {
-            var event = createEvent(type);
-            event.data = data;
-            var objects = [];
-            for (var i = 0; i < this.targets.length; i++) {
-                this.enumerate(this.targets[i], function(obj, property) {
-                    if (type in property && objects.indexOf(obj) === -1) {
-                        obj.dispatchEvent(event);
-                        objects.push(obj);
-                    }
-                });
-            }
-        }
-    }).call(EventModel.prototype);
+(function (edm) {
 
-    function SelectorEventModel(model) {
-        EventModel.call(this, model);
-    }
-    (function() {
-        this.enumerate = function(target, callback) {
-            for (var _property in this.model) {
-                var elements = target.querySelectorAll(_property);
-                for (var i = 0; i < elements.length; i++) {
-                    callback(elements[i], this.model[_property]);
-                }
-            }
-        }
-    }).call(SelectorEventModel.prototype = new EventModel());
+	function forEach(seq, fn) {
+		if ("length" in seq)
+			for (var i = 0; i < seq.length; i++)
+				fn(seq[i], i);
+		else for (var x in seq)
+			fn(seq[x], x);
+	}
 
-    global.EventModel = EventModel;
-    global.SelectorEventModel = SelectorEventModel;
-}(window));
+	function Model(viewModel) {
+		this.viewmodel = viewModel;
+	}
+
+	(function () {
+
+		function wrapper(handler, model, event) {
+			event.result = handler.call(this, event, model);
+		}
+
+		function delegate(query, handler, model, event) {
+			var elements = Array.prototype.slice.call(this.querySelectorAll(query));
+			if (elements.indexOf(event.target) > -1)
+				event.result = handler.call(event.target, event, model);
+		}
+
+		this.bind = function () {
+			var model = this;
+			forEach(this.viewmodel, function (item, selector) {
+				if (selector.indexOf(";") > -1) {
+					var s = selector.split(";");
+					forEach(document.querySelectorAll(s[0]), function (root) {
+						forEach(item, function (handler, action) {
+							root.addEventListener(action, delegate.bind(root, s[1], handler, model));
+							if (!(action in model))
+								model[action] = model.trigger.bind(model, action);
+						});
+					});
+				} else {
+					forEach(document.querySelectorAll(selector), function (element) {
+						forEach(item, function (handler, action) {
+							element.addEventListener(action, wrapper.bind(element, handler, model));
+							if (!(action in model))
+								model[action] = model.trigger.bind(model, action);
+						});
+					});
+				}
+			});
+		};
+
+		this.trigger = function (name, data) {
+			var e = new Event(name, { bubbles: true, cancelable: true });
+			e.data = data;
+			var triggered = [];
+			var results = [];
+			forEach(this.viewmodel, function (item, selector) {
+				if (!(name in item)) return;
+				if (selector.indexOf(";") > -1) {
+					var s = selector.split(";");
+					forEach(document.querySelectorAll(s[0]), function (root) {
+						forEach(root.querySelectorAll(s[1]), function (element) {
+							if (triggered.indexOf(element) > -1) return;
+							e.result = null;
+							element.dispatchEvent(e);
+							if (e.result)
+								results.push(e.result);
+							triggered.push(element);
+						});
+					});
+				} else {
+					forEach(document.querySelectorAll(selector), function (element) {
+						if (triggered.indexOf(element) > -1) return;
+						e.result = null;
+						element.dispatchEvent(e);
+						if (e.result)
+							results.push(e.result);
+						triggered.push(element);
+					});
+				}
+			});
+			if (results.length > 1)
+				return results;
+			else if (results.length == 1)
+				return results[0];
+			return null;
+		}
+
+	}).call(Model.prototype)
+
+	edm.Model = Model;
+
+} (window.edm = {}));
